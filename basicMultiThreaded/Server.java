1ebf483d2d05c6ede9a4d46f0681ad366e3bf21e
Ergi SALA <ergisala@live.com>
2019-02-01 10:49:42 +0100

    point 3.1 v1

--- basicMultiThreaded/Server.java
+++ basicMultiThreaded/Server.java
@@ -1,5 +1,22 @@
 package basicMultiThreaded;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+	private static final int port = 2020;
+	private static ServerSocket server;
+
+	public static void main(String args[]) throws IOException {
+		server = new ServerSocket(port);
+		int id = 1;
+		while (true) {
+			Socket client = server.accept();
+			//System.out.println(client.);
+			new Worker(client, id);
+			System.out.println("Worker " + id + " created");
+			id++;
+		}
+	}
 }
